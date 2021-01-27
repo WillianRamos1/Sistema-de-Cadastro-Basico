9d742835571178e64aa3def8b7f9d0dc433824a4
Willian <willianlive73@gmail.com>
2021-01-27 18:39:48 -0300

    Primeiro projeto usando Windows Forms

---
+++ Funcionarios.Designer.cs
@@ -0,0 +1,341 @@
+﻿
+namespace CRUD
+{
+    partial class Funcionarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Salvar = new System.Windows.Forms.Button();
+            this.btn_Buscar = new System.Windows.Forms.Button();
+            this.btn_Editar = new System.Windows.Forms.Button();
+            this.btn_Excluir = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mskTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mskCelular = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtRG = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pesquisar Nome:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(124, 104);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(436, 20);
+            this.txtPesquisa.TabIndex = 1;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Location = new System.Drawing.Point(12, 12);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 58);
+            this.btn_Add.TabIndex = 2;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            // 
+            // btn_Salvar
+            // 
+            this.btn_Salvar.Location = new System.Drawing.Point(136, 12);
+            this.btn_Salvar.Name = "btn_Salvar";
+            this.btn_Salvar.Size = new System.Drawing.Size(75, 58);
+            this.btn_Salvar.TabIndex = 3;
+            this.btn_Salvar.Text = "Salvar";
+            this.btn_Salvar.UseVisualStyleBackColor = true;
+            this.btn_Salvar.Click += new System.EventHandler(this.btn_Salvar_Click);
+            // 
+            // btn_Buscar
+            // 
+            this.btn_Buscar.Location = new System.Drawing.Point(266, 12);
+            this.btn_Buscar.Name = "btn_Buscar";
+            this.btn_Buscar.Size = new System.Drawing.Size(75, 58);
+            this.btn_Buscar.TabIndex = 4;
+            this.btn_Buscar.Text = "Buscar";
+            this.btn_Buscar.UseVisualStyleBackColor = true;
+            this.btn_Buscar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_Editar
+            // 
+            this.btn_Editar.Location = new System.Drawing.Point(411, 12);
+            this.btn_Editar.Name = "btn_Editar";
+            this.btn_Editar.Size = new System.Drawing.Size(75, 58);
+            this.btn_Editar.TabIndex = 5;
+            this.btn_Editar.Text = "Editar";
+            this.btn_Editar.UseVisualStyleBackColor = true;
+            this.btn_Editar.Click += new System.EventHandler(this.btn_Editar_Click);
+            // 
+            // btn_Excluir
+            // 
+            this.btn_Excluir.Location = new System.Drawing.Point(545, 12);
+            this.btn_Excluir.Name = "btn_Excluir";
+            this.btn_Excluir.Size = new System.Drawing.Size(75, 58);
+            this.btn_Excluir.TabIndex = 6;
+            this.btn_Excluir.Text = "Excluir";
+            this.btn_Excluir.UseVisualStyleBackColor = true;
+            this.btn_Excluir.Click += new System.EventHandler(this.btn_Excluir_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(78, 151);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(482, 20);
+            this.txtNome.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Telefone:";
+            // 
+            // mskTelefone
+            // 
+            this.mskTelefone.Location = new System.Drawing.Point(92, 202);
+            this.mskTelefone.Mask = "(00) 0 0000-0000";
+            this.mskTelefone.Name = "mskTelefone";
+            this.mskTelefone.Size = new System.Drawing.Size(111, 20);
+            this.mskTelefone.TabIndex = 10;
+            this.mskTelefone.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(280, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Celular:";
+            // 
+            // mskCelular
+            // 
+            this.mskCelular.Location = new System.Drawing.Point(328, 202);
+            this.mskCelular.Mask = "(00) 0 0000-0000";
+            this.mskCelular.Name = "mskCelular";
+            this.mskCelular.Size = new System.Drawing.Size(111, 20);
+            this.mskCelular.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "E-mail:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 299);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Endereço:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(78, 250);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(482, 20);
+            this.txtEmail.TabIndex = 14;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(96, 296);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(464, 20);
+            this.txtEndereco.TabIndex = 16;
+            this.txtEndereco.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 344);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "N°:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(62, 341);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(94, 20);
+            this.txtNumero.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(234, 344);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Bairro:";
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(277, 341);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(283, 20);
+            this.txtBairro.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(34, 383);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(26, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "RG:";
+            // 
+            // txtRG
+            // 
+            this.txtRG.Location = new System.Drawing.Point(66, 380);
+            this.txtRG.Name = "txtRG";
+            this.txtRG.Size = new System.Drawing.Size(190, 20);
+            this.txtRG.TabIndex = 22;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(295, 383);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(30, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "CPF:";
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(331, 380);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(229, 20);
+            this.txtCPF.TabIndex = 24;
+            this.txtCPF.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // Funcionarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 450);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtRG);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.mskCelular);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.mskTelefone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_Excluir);
+            this.Controls.Add(this.btn_Editar);
+            this.Controls.Add(this.btn_Buscar);
+            this.Controls.Add(this.btn_Salvar);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.txtPesquisa);
+            this.Controls.Add(this.label1);
+            this.Name = "Funcionarios";
+            this.Text = "Funcionarios";
+            this.Load += new System.EventHandler(this.Funcionarios_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Salvar;
+        private System.Windows.Forms.Button btn_Buscar;
+        private System.Windows.Forms.Button btn_Editar;
+        private System.Windows.Forms.Button btn_Excluir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox mskTelefone;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox mskCelular;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtRG;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtCPF;
+    }
+}
---
+++ Funcionarios.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace CRUD
+{
+    public partial class Funcionarios : Form
+    {
+        public Funcionarios()
+        {
+            InitializeComponent();
+
+            /*
+            txtPesquisa.Enabled = false;
+            txtNome.Enabled = false;
+            mskTelefone.Enabled = false;
+            mskCelular.Enabled = false;
+            txtEmail.Enabled = false;
+            txtEndereco.Enabled = false;
+            txtNumero.Enabled = false;
+            txtBairro.Enabled = false;
+            txtRG.Enabled = false;
+            txtCPF.Enabled = false;
+            */
+        }
+
+
+        SqlConnection sqlCon = null;
+        private string strCon = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Willian;Data Source=DESKTOP-79R9K1O\SQLEXPRESS";
+        private string strSql = string.Empty;
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            strSql = "select * from Funcionarios where Nome=@Pesquisa";
+
+            sqlCon = new SqlConnection(strCon);
+
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+            comando.Parameters.Add("@Pesquisa", SqlDbType.VarChar).Value = txtPesquisa.Text;
+
+            try
+            {
+
+                if (txtPesquisa.Text == string.Empty)
+                {
+                    MessageBox.Show("Voce precisa digitar um nome");
+                }
+
+                sqlCon.Open();
+
+                SqlDataReader dr = comando.ExecuteReader();
+
+                if (dr.HasRows == false)
+                {
+                    throw new Exception("Esse nome não esta cadastrado");
+                }
+
+                dr.Read();
+
+                txtNome.Text = Convert.ToString(dr["Nome"]);
+                mskTelefone.Text = Convert.ToString(dr["Telefone"]);
+                mskCelular.Text = Convert.ToString(dr["Celular"]);
+                txtEmail.Text = Convert.ToString(dr["Email"]);
+                txtEndereco.Text = Convert.ToString(dr["Endereco"]);
+                txtNumero.Text = Convert.ToString(dr["Numero"]);
+                txtBairro.Text = Convert.ToString(dr["Bairro"]);
+                txtRG.Text = Convert.ToString(dr["RG"]);
+                txtCPF.Text = Convert.ToString(dr["CPF"]);
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            finally {
+
+                sqlCon.Close();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Funcionarios_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Salvar_Click(object sender, EventArgs e)
+        {
+            strSql = "insert into Funcionarios(Nome, Telefone, Celular, Email, Endereco, Numero, Bairro, RG, CPF)values(@Nome, @Telefone, @Celular, @Email, @Endereco, @Numero, @Bairro, @RG, @CPF)";
+
+            sqlCon = new SqlConnection(strCon);
+
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+
+            comando.Parameters.Add("@Nome", SqlDbType.VarChar).Value = txtNome.Text;
+            comando.Parameters.Add("@Telefone", SqlDbType.VarChar).Value = mskTelefone.Text;
+            comando.Parameters.Add("@Celular", SqlDbType.VarChar).Value = mskCelular.Text;
+            comando.Parameters.Add("@Email", SqlDbType.VarChar).Value = txtEmail.Text;
+            comando.Parameters.Add("@Endereco", SqlDbType.VarChar).Value = txtEndereco.Text;
+            comando.Parameters.Add("@Numero", SqlDbType.VarChar).Value = txtNumero.Text;
+            comando.Parameters.Add("@Bairro", SqlDbType.VarChar).Value = txtBairro.Text;
+            comando.Parameters.Add("@RG", SqlDbType.VarChar).Value = txtRG.Text;
+            comando.Parameters.Add("@CPF", SqlDbType.VarChar).Value = txtCPF.Text;
+
+            try
+            {
+                sqlCon.Open();
+
+                comando.ExecuteNonQuery();
+
+                MessageBox.Show("Cadastro efetuado com sucesso");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            finally
+            {
+                sqlCon.Close();
+            }
+
+
+        }
+
+        private void btn_Editar_Click(object sender, EventArgs e)
+        {
+            strSql = "update Funcionarios set Nome=@Nome, Telefone=@Telefone, Celular=@Celular, Email=@Email, Endereco=@Endereco, Numero=@Numero, Bairro=@Bairro, RG=@RG, CPF=@CPF";
+
+            sqlCon = new SqlConnection(strCon);
+
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+
+            comando.Parameters.Add("@Nome", SqlDbType.VarChar).Value = txtNome.Text;
+            comando.Parameters.Add("@Telefone", SqlDbType.VarChar).Value = mskTelefone.Text;
+            comando.Parameters.Add("@Celular", SqlDbType.VarChar).Value = mskCelular.Text;
+            comando.Parameters.Add("@Email", SqlDbType.VarChar).Value = txtEmail.Text;
+            comando.Parameters.Add("@Endereco", SqlDbType.VarChar).Value = txtEndereco.Text;
+            comando.Parameters.Add("@Numero", SqlDbType.VarChar).Value = txtNumero.Text;
+            comando.Parameters.Add("@Bairro", SqlDbType.VarChar).Value = txtBairro.Text;
+            comando.Parameters.Add("@RG", SqlDbType.VarChar).Value = txtRG.Text;
+            comando.Parameters.Add("@CPF", SqlDbType.VarChar).Value = txtCPF.Text;
+
+
+            try
+            {
+
+                sqlCon.Open();
+
+                comando.ExecuteNonQuery();
+
+                MessageBox.Show("Cadastro atualizado com sucesso");
+            }
+
+            catch (Exception ex)
+
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+            finally
+            {
+
+                sqlCon.Close();
+            }
+
+        }
+
+        private void btn_Excluir_Click(object sender, EventArgs e)
+        {
+            strSql = "delete from Funcionarios where Nome=@Nome";
+
+            sqlCon = new SqlConnection(strCon);
+
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+            comando.Parameters.Add("@Nome", SqlDbType.VarChar).Value = txtNome.Text;
+
+            try
+            {
+                sqlCon.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Cadastro excluido com sucesso");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            finally
+            { 
+               sqlCon.Close();
+            }
+        }
+    }
+}
